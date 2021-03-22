84084f30156b4c86ba0cd44cbf3151a95d0138b8
Guy Indepurker <guy235@gmail.com>
2021-03-22 14:58:22 +0200

    clean pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './styles/_style.scss';
 
-import Header from './cmps/Header/Header';
 import Footer from './cmps/Footer/Footer';
 import HomePage from './pages/HomePage/HomePage';
-import About from './pages/About/About';
 
 function App() {
   return (
